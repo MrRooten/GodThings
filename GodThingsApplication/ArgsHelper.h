e986242c5e676a24a030de8079e9714d5ae7ac87
thx <elliot10156@gmail.com>
2022-11-30 00:02:34 +0800

    Remove xlsx module

--- GodThingsApplication/ArgsHelper.h
+++ GodThingsApplication/ArgsHelper.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
 #include <set>
-#include <xlsxwriter.h>
 #include "PythonUtils.h"
 #include "FileInfo.h"
 #include "utils.h"
@@ -258,33 +257,6 @@ public:
 #endif // PYTHON_ENABLE
 		}
 		else if (subcmd == L"test") {
-			lxw_workbook* workbook = workbook_new("demo.xlsx");
-			lxw_worksheet* worksheet = workbook_add_worksheet(workbook, NULL);
-
-			/* Add a format. */
-			lxw_format* format = workbook_add_format(workbook);
-
-			/* Set the bold property for the format */
-			format_set_bold(format);
-
-			/* Change the column width for clarity. */
-			worksheet_set_column(worksheet, 0, 0, 20, NULL);
-
-			/* Write some simple text. */
-			worksheet_write_string(worksheet, 0, 0, "Hello", NULL);
-
-			/* Text with formatting. */
-			worksheet_write_string(worksheet, 1, 0, "World", format);
-
-			/* Write some numbers. */
-			worksheet_write_number(worksheet, 2, 0, 123, NULL);
-			worksheet_write_number(worksheet, 3, 0, 123.456, NULL);
-
-			/* Insert an image. */
-			//worksheet_insert_image(worksheet, 1, 2, "logo.png");
-
-			workbook_close(workbook);
-
 			return ;
 		}
 		else if (subcmd == L"list_path") {
